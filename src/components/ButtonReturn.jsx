fd8aabd03290d2abccc9506477fef87b3e5a238f
unknown <aratavalga@gmail.com>
2025-02-24 19:06:21 -0500

    reset

--- src/components/ButtonReturn.jsx
+++
@@ -1,5 +0,0 @@
-export const ButtonReturn = ({ name = "null" }) => {
-    return (
-        <button className="border-2 border-rebecca_purple rounded-full px-12 py-1 text-rebecca_purple font-rokkitt text-2xl hover:bg-rebecca_purple hover:text-white">{name}</button>
-    )
-}
--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-export const Card = () => {
-    return (
-        <article className="flex flex-col gap-4">
-            <h4 className="text-[31px]">Lorem ipsum dolor <br /> sit amet</h4>
-            <p className="text-lg text-gray-600">Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. <br /> Amet, turpis suspendisse ut <br /> semper ipsum enim.</p>
-            <span className="text-rebecca_purple text-[21px]">Lorem ipsum</span>
-        </article>
-    )
-}
