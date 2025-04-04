d227992e79f00c20a27149f27cb5a7ec5fb04850
Tomoko <aratavalga@gmail.com>
2025-04-04 06:58:21 -0500

    plus

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+    return (
+        <nav className="flex items-center min-h-[4rem] bg-[#D0C7E6] justify-around">
+            <h1>Youth</h1>
+            <ul className="flex gap-x-4 uppercase">
+                <li>Home</li>
+                <li>About</li>
+                <li>Products</li>
+                <li>Contacts</li>
+            </ul>
+        </nav>
+
+    )
+}
