cda7334c6289cd5398b399cbecc9c2b1db935d84
Tomoko <aratavalga@gmail.com>
2025-04-06 14:50:20 -0500

    Nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 export const Navbar = () => {
     return (
-        <nav className="flex items-center min-h-[4rem] bg-[#D0C7E6] justify-around">
-            <h1>Youth</h1>
-            <ul className="flex gap-x-4 uppercase">
+        <nav className="flex items-center min-h-[5rem] bg-[#D0C7E6] justify-around">
+            <h1 className="text-3xl">Youth</h1>
+            <ul className="flex gap-x-10 uppercase">
                 <li>Home</li>
                 <li>About</li>
                 <li>Products</li>
